d62d300f1c9cacd59a46d1409d2447482046b567
TanishqNegi4u <tanishqnegi766854@gmail.com>
2026-02-18 15:38:11 +0530

    Create backend/src/main/java/com/shopwave/model/Cart.java

---
+++ backend/src/main/java/com/shopwave/model/Cart.java
@@ -0,0 +1,22 @@
+package com.shopwave.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> items = new ArrayList<>();
+}
